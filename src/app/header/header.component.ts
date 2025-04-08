e142928457a7a03f47d6af49528cdaa34bcc8b40
Marius Winterscheid <44503013+MariusWinterscheid@users.noreply.github.com>
2025-04-08 20:05:53 +0200

    build navigation

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,74 @@
+let isNavOpen = false;
+let isLenEn = true;
+
+const IMAGES_BURGER_MENU_OPEN = [
+  'assets/img/icons/burgerMenuTransition/open/burgerMenuOpen1.png',
+  'assets/img/icons/burgerMenuTransition/open/burgerMenuOpen2.png',
+  'assets/img/icons/burgerMenuTransition/open/burgerMenuOpen3.png',
+  'assets/img/icons/burgerMenuTransition/open/burgerMenuOpen4.png'
+];
+const IMAGES_BURGER_MENU_CLOSE = [
+  'assets/img/icons/burgerMenuTransition/close/burgerMenuClose1.png',
+  'assets/img/icons/burgerMenuTransition/close/burgerMenuClose2.png',
+  'assets/img/icons/burgerMenuTransition/close/burgerMenuClose3.png',
+  'assets/img/icons/burgerMenu.png'
+];
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  onClickLenSwitcher(){
+    const lenSwitcherEn = document.getElementById('lenSwitcherEn') as HTMLImageElement;
+    const lenSwitcherDe = document.getElementById('lenSwitcherDe') as HTMLImageElement;
+    if (!lenSwitcherEn || !lenSwitcherDe) return;
+
+    lenSwitcherEn.classList.toggle('lan-active');
+    lenSwitcherDe.classList.toggle('lan-active');
+
+    isLenEn = !isLenEn;
+
+    console.log('Language switched to: ' + (isLenEn ? 'English' : 'German'));
+  }
+
+  onClickBurgerMenu(){
+    this.animateBurgerMenu();
+    this.toogleLenSwitcherVisibility();
+    //Function for open or close the navbar
+  }
+
+  toogleLenSwitcherVisibility(){
+    const lenSwitcherMain = document.getElementById('lanSwitcherMain') as HTMLImageElement;
+    if (!lenSwitcherMain) return;
+    lenSwitcherMain.style.left = isNavOpen ? '0' : '150dvw';
+  }
+
+  animateBurgerMenu(){
+    let path = 'assets/img/icons/burgerMenu.png';
+    const burgerMenu = document.getElementById('burgerMenu') as HTMLImageElement;
+
+    if (!burgerMenu) return;
+    this.playAnimationBurgerMenu(path, burgerMenu);
+
+    isNavOpen = !isNavOpen;
+    console.log('Navbar is now ' + (isNavOpen ? 'open' : 'closed'));
+  }
+
+  playAnimationBurgerMenu(path: string, burgerMenu: HTMLImageElement) {
+    const choosenArray = isNavOpen ? IMAGES_BURGER_MENU_CLOSE : IMAGES_BURGER_MENU_OPEN;
+    for (let i = 0; i < choosenArray.length; i++) {
+      setTimeout(() => {
+        path = choosenArray[i];
+        burgerMenu.src = path;
+      }, i * 100);
+    }
+  }
+
+}
