7e06bd07b4f3c00dc24ee6a684ec3cb8029abcf1
Marius Winterscheid <44503013+MariusWinterscheid@users.noreply.github.com>
2025-04-18 23:32:22 +0200

    worked on contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,36 @@
 import { Component, inject } from '@angular/core';
 import { TextService } from '../shared/services/text.service';
 import { SettingsService } from '../shared/services/settings.service';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   txtService = inject(TextService);
   settingsService = inject(SettingsService);
+
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      policy: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Formular abgesendet:', this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched(); // zeigt sofort alle Fehler an
+    }
+  }
 }
