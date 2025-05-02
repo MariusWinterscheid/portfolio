f8933f570b6f5a3536d696a251ffe2b38e95ff4b
Marius Winterscheid <44503013+MariusWinterscheid@users.noreply.github.com>
2025-05-02 13:38:59 +0200

    refactored contact form, add php

--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -1,38 +1,51 @@
 import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { TextService } from '../../shared/services/text.service';
 import { GlobalStatesService } from '../../shared/services/global-states.service';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    FooterComponent
+    FooterComponent,
+    FormsModule
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
   txtService = inject(TextService);
+  http = inject(HttpClient);
+  mailTest = true;
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    policyCkecked: false
+  }
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
   currentLang: 'en' | 'de' = 'en';
   private sub: Subscription | null = null;
 
-  contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private statesService: GlobalStatesService) {
-    this.contactForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      message: ['', Validators.required],
-      policy: [false, Validators.requiredTrue]
-    });
-  }
+  constructor(private statesService: GlobalStatesService) {}
 
   ngOnInit(): void {
     this.sub = this.statesService.currentLang$.subscribe((lang) => {
@@ -45,12 +58,22 @@ export class ContactComponent {
     }
   }
 
-  onSubmit() {
-    if (this.contactForm.valid) {
-      console.log('Formular abgesendet:', this.contactForm.value);
-      this.contactForm.reset();
-    } else {
-      this.contactForm.markAllAsTouched();
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.submitted && contactForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            /* Show Userfeedback? */
+            contactForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (contactForm.submitted && contactForm.form.valid && this.mailTest) {
+      console.log('Form data:', this.contactData);
+      contactForm.resetForm();
     }
   }
 }
