be0e38fd71efe533fc0636e78bf01ce001881734
MariusWinterscheid <winterscheid.edv@gmail.com>
2025-07-26 21:42:03 +0200

    done routing for main page, imprint and privacy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PrivacyComponent } from './shared/components/privacy/privacy.component';
+import { TestComponent } from './test/test.component';
+import { LegalComponent } from './shared/components/legal/legal.component';
 
 export const routes: Routes = [
-
+    {path:'', component: MainComponent},
+    {path:'imprint', component: PrivacyComponent},
+    {path:'legal', component: LegalComponent},
 ];
