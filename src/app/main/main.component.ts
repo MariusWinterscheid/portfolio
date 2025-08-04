710eb1bbfacc88b8a1121b2438db55877a4f4715
MariusWinterscheid <winterscheid.lindlar@gmail.com>
2025-08-04 16:27:51 +0200

    privacy responsive design

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,6 +6,7 @@ import { SkillsComponent } from '../sections/skills/skills.component';
 import { PortfolioComponent } from '../sections/portfolio/portfolio.component';
 import { ReferencesComponent } from '../sections/references/references.component';
 import { ContactComponent } from '../sections/contact/contact.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
@@ -17,7 +18,8 @@ import { ContactComponent } from '../sections/contact/contact.component';
     SkillsComponent,
     PortfolioComponent,
     ReferencesComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
